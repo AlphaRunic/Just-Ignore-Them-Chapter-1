e4a1dbc6808c441f6e5e402c1fc92915fddf2000
AlphaRunic <peenreee@gmail.com>
2021-05-02 01:25:49 +0100

    Fix ViewModel component

--- src/client/classes/GameComponents/ViewModel.ts
+++ src/client/classes/GameComponents/ViewModel.ts
@@ -1,9 +1,12 @@
 import { Carbon as Framework, Camera, Assets } from "shared/Carbon";
 import { BaseComponent } from "shared/Component";
 
+const { rad } = math;
+
 class Offsets {
     public static Final =  new CFrame();
-    public static ViewModel =  new CFrame(0, -.1, 1.5);
+    public static ViewModel =  new CFrame(0, -.1, .3);
+    public static ViewModelAngular = CFrame.Angles(-rad(30), 0, 0);
 }
 
 export class ViewModel implements BaseComponent {
@@ -11,10 +14,15 @@ export class ViewModel implements BaseComponent {
     
     private vm: Model = Assets.ViewModel.Clone();
     private flashlight: Model = Assets.Flashlight.Clone();
+
+    public Start(): void {
+        this.vm.Parent = Camera;
+    }
     
     public Update(dt: number): void {
         Offsets.Final = Camera.CFrame
-            .mul(Offsets.ViewModel);
+            .mul(Offsets.ViewModel)
+            .mul(Offsets.ViewModelAngular);
         
         this.vm.SetPrimaryPartCFrame(Offsets.Final)
     }
